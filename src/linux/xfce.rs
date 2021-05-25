557a4b3fe44905ace3054718ef21597bcb960ef7
MGlolenstine <MGlolenstine@gmail.com>
2021-05-25 09:38:54 +0200

    Xfce fixes (#3)
    * Moved XFCE to its own module.
    
    * Added multiple path changes.
    
    * Added path debug info.
    
    * Fixed paths.
    
    * Fixed paths.
    
    * Working on XFCE.

---
+++ src/linux/xfce.rs
@@ -0,0 +1,41 @@
+use crate::get_stdout;
+use crate::run;
+use Result;
+
+/// Returns the wallpaper of XFCE.
+pub fn get() -> Result<String, Box<dyn std::error::Error>> {
+    let mut ret = String::new();
+    for xfce_path in get_last_image_paths()? {
+        ret = format!(
+            "{},{}",
+            ret,
+            get_stdout("xfconf-query", &["-c", "xfce4-desktop", "-p", &xfce_path])?
+        )
+    }
+    Ok(ret)
+}
+
+/// Sets the wallpaper for XFCE.
+pub fn set(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    for xfce_path in get_last_image_paths()? {
+        run(
+            "xfconf-query",
+            &["-c", "xfce4-desktop", "-p", &xfce_path, "-s", &path],
+        )?
+    }
+    Ok(())
+}
+
+fn get_last_image_paths() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let str = get_stdout("xfconf-query", &["-l", "-c", "xfce4-desktop"])?;
+    Ok(str
+        .split("\n")
+        .filter(|s| s.ends_with("/last-image"))
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>())
+}
+
+#[test]
+fn test_get_wallpaper() {
+    dbg!(crate::linux::xfce::get().unwrap());
+}
